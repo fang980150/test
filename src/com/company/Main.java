1f56494302b769e528df3e00f327701666f70cec
fang <fangqiang0627@163.com>
2020-07-12 00:04:08 +0800

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String a="hello";
+        if (a.equals("f")){
+            System.out.println("1");
+        }
+        else {
+            System.out.println("2");
+        }
+        if (a.equals("hello")){
+            System.out.println("3");
+        }
+
+    }
+}
